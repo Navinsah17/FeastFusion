01f84d7f19afe6a51b515d4bf7a2d8f0fde9c0fd
Navin Sah <navinkrsah17@gmail.com>
2024-02-17 13:52:09 +0530

    first commit

---
+++ app/src/main/java/com/example/feastfusion/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.feastfusion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.feastfusion.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment= supportFragmentManager.findFragmentById(R.id.navhostfragment) as NavHostFragment
+        val navController= navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+    }
+}
